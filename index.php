fb804b69164acbb3f7abb445d4faaab723ce8cdf
show981111 <show981111@gmail.com>
2020-07-26 23:10:41 +0900

    implement routing

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php phpinfo();?>
+<?php 
+	
+	header('Content-Type: application/json; charset=UTF-8'); 
+	header("HTTP/1.1 200 OK"); 
+	header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+
+	error_reporting(E_ALL);
+
+	ini_set("display_errors", 1);
+
+	echo 'index called!';
+	$request = $_SERVER['REQUEST_URI'];
+	
+	$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	$uri = explode('/', $uri);
+
+	if ($uri[1] !== 'user') { 
+		echo 'nothing!';
+		//header("HTTP/1.1 404 Not Found"); 
+		exit(); 
+	}
+
+	$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+	// switch ($requestMethod) {
+	// 	case 'POST':
+	// 		require_once('getUserInfo.php');
+	// 		break;
+		
+	// 	default:
+	// 		# code...
+	// 		break;
+	// }
+
+?>
